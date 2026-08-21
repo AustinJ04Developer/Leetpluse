8ec778c790ffc18cdea83d08356d03f6d1ca6ec6
Austin J <austinjustin4809@gmail.com>
2026-08-21 23:28:23 +0530

    Minor correccction

--- Client/src/pages/admin/StudentManagementPage.jsx
+++ Client/src/pages/admin/StudentManagementPage.jsx
@@ -401,6 +401,7 @@ const StudentManagementPage = () => {
                   className="w-full px-3 py-2 bg-slate-900 border border-slate-800 rounded-xl text-xs text-white focus:outline-none focus:border-indigo-500"
                 />
               </div>
+              <div className="grid grid-cols-2 gap-3">
                 <div>
                   <label className="block text-xs font-semibold text-slate-300 mb-1">Year Level</label>
                   <select
